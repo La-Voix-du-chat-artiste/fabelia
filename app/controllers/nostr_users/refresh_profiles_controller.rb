d175a5e2f38b57cafaf59304a25daa9c3d369591
Anthony Robin <contact@anthony-robin.fr>
2023-08-09 22:22:25 +0200

    Add action to refresh `nostr_user` metadata
    If metadata have changed on remote relays, this new action will refresh old data.

---
+++ app/controllers/nostr_users/refresh_profiles_controller.rb
@@ -0,0 +1,20 @@
+module NostrUsers
+  class RefreshProfilesController < ApplicationController
+    before_action :set_nostr_user
+
+    # @route POST /nostr_users/:id/refresh_profiles (refresh_profiles)
+    def create
+      response = NostrServices::FetchProfile.call(@nostr_user)
+
+      @nostr_user.update(metadata_response: response)
+
+      redirect_to nostr_users_path, notice: 'Les métadonnées du profil ont bien été rafraîchies'
+    end
+
+    private
+
+    def set_nostr_user
+      @nostr_user = NostrUser.find(params[:id])
+    end
+  end
+end
