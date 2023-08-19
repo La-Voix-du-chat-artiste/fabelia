081214698f2970399e152f9b2cbba2300d4bc476
Anthony Robin <contact@anthony-robin.fr>
2023-08-19 23:16:43 +0200

    Fix Rubocop offense on policy spec

--- spec/policies/story_policy_spec.rb
+++ spec/policies/story_policy_spec.rb
@@ -6,17 +6,19 @@ RSpec.describe StoryPolicy do
 
   let(:context) { { user: user } }
 
-  describe_rule :create? do
-    succeed 'when user is super admin' do
-      let(:role) { :super_admin }
-    end
+  %i[create? destroy?].each do |action|
+    describe_rule action do
+      succeed 'when user is super admin' do
+        let(:role) { :super_admin }
+      end
 
-    succeed 'when user is admin' do
-      let(:role) { :admin }
-    end
+      succeed 'when user is admin' do
+        let(:role) { :admin }
+      end
 
-    failed 'when user is standard' do
-      let(:role) { :standard }
+      failed 'when user is standard' do
+        let(:role) { :standard }
+      end
     end
   end
 
@@ -27,20 +29,6 @@ RSpec.describe StoryPolicy do
       failed 'when adventure is ended' do
         before { record.adventure_ended_at = 1.day.ago }
       end
-    end
-
-    succeed 'when user is admin' do
-      let(:role) { :admin }
-    end
-
-    failed 'when user is standard' do
-      let(:role) { :standard }
-    end
-  end
-
-  describe_rule :destroy? do
-    succeed 'when user is super admin' do
-      let(:role) { :super_admin }
     end
 
     succeed 'when user is admin' do
