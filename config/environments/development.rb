6772408429bb4689ba5e6a22605aefd5def355bd
Anthony Robin <contact@anthony-robin.fr>
2023-10-29 20:10:47 +0100

    Upgrade properly to Rails 7.1
    Run `$ bin/rails app:update` to add missing config and files.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,7 +6,7 @@ Rails.application.configure do
   # In the development environment your application's code is reloaded any time
   # it changes. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
-  config.cache_classes = false
+  config.enable_reloading = true
 
   # Do not eager load code on boot.
   config.eager_load = false
@@ -16,6 +16,9 @@ Rails.application.configure do
 
   # Enable server timing
   config.server_timing = true
+
+  port = ENV.fetch('PORT', 3000)
+  host = ENV.fetch('HOST', 'http://localhost')
 
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
@@ -33,18 +36,12 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
-  port = ENV.fetch('PORT', 3000)
-  host = ENV.fetch('HOST', 'http://localhost')
-  config.action_controller.default_url_options = {
-    host: host, port: port
-  }
+  config.action_controller.default_url_options = { host: host, port: port }
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
-  config.action_mailer.default_url_options = {
-    host: 'http://localhost', port: ENV.fetch('PORT', 3000)
-  }
+  config.action_mailer.default_url_options = { host: host, port: port }
   config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.perform_deliveries = true
   # Don't care if the mailer can't send.
@@ -67,6 +64,9 @@ Rails.application.configure do
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
 
+  # Highlight code that enqueued background job in logs.
+  config.active_job.verbose_enqueue_logs = true
+
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
@@ -78,6 +78,9 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  # Raise error when a before_action's only/except options reference missing actions
+  config.action_controller.raise_on_missing_callback_actions = true
 
   config.active_job.queue_adapter = :sidekiq
 
--- config/initializers/support.rb
+++ config/initializers/support.rb
@@ -1,5 +1,5 @@
 require 'retry'
-require 'nostr_override'
+require 'nostr'
 
 %w[core_ext].each do |folder|
   Dir[Rails.root.join('lib', folder, '**', '*.rb')].each { |f| require f }
