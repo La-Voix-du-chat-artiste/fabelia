0ffa113b4a88237a5cfca130fba0d35931e3cc07
Anthony Robin <contact@anthony-robin.fr>
2023-11-18 17:46:07 +0100

    Update header subtitle displaying random quotes

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -16,7 +16,23 @@ module.exports = {
       colors: {
         'primary-color': 'var(--color-primary)',
         'secondary-color': 'var(--color-secondary)'
-      }
+      },
+      animation: {
+        typing: 'typing 3s steps(33)',
+      },
+      keyframes: {
+        typing: {
+          '0%': {
+            width: '0',
+          },
+          '80%': {
+            width: '33ch',
+          },
+          '100%': {
+            width: '33ch',
+          },
+        },
+      },
     },
   },
   plugins: [
