2255d6f8c6c28d1e853b5476a9b9d0102d29a5d1
Anthony Robin <contact@anthony-robin.fr>
2023-12-27 19:11:51 +0100

    Replace `sidekiq` by `solid_queue` job adapter
    `SolidQueue` will become the new default job adapter in Rails 8.
    
    @see https://github.com/rails/rails/issues/50442
    
    Details:
    - Remove `sidekiq` gem and related configuration
    - Remove useless `redis` and `network` containers
    - Add `solid_queue` gem with related configuration and migration

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -82,7 +82,7 @@ Rails.application.configure do
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
 
-  config.active_job.queue_adapter = :sidekiq
+  config.active_job.queue_adapter = :solid_queue
 
   config.hosts << ENV['HOST'] if ENV['HOST'].present?
 
